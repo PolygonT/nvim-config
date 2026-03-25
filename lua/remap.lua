ba639c5ef3860a12e01cb48c37d0db4e1ece3dcc
WSL-Wenhaoxiong <formalityefqaq@gmail.com>
2026-03-26 02:16:05 +0800

    update

--- lua/remap.lua
+++ lua/remap.lua
@@ -1,27 +1,27 @@
 vim.g.mapleader = " "
 
-vim.keymap.set("n", "<leader>w", "<C-w>") 
+vim.keymap.set("n", "<leader>w", "<C-w>")
 
-vim.keymap.set("n", "H", "^") 
-vim.keymap.set("v", "H", "^") 
-vim.keymap.set("n", "L", "$") 
-vim.keymap.set("v", "L", "$") 
+vim.keymap.set("n", "H", "^")
+vim.keymap.set("v", "H", "^")
+vim.keymap.set("n", "L", "$")
+vim.keymap.set("v", "L", "$")
 
 -- vim.keymap.set("n", "<C-d>", "<C-d>zz") 
 -- vim.keymap.set("n", "<C-u>", "<C-u>zz") 
 
-vim.keymap.set("v", "<leader>y", [["+y]]) 
-vim.keymap.set("n", "<leader>y", [["+y]]) 
-vim.keymap.set("x", "<leader>p", [["_dP]]) 
-vim.keymap.set("v", "<leader>p", [["_dP]]) 
+vim.keymap.set("v", "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>y", [["+y]])
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("v", "<leader>p", [["_dP]])
 
-vim.keymap.set("i", "<C-c>", "<Esc>") 
+vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- terminal relate
 -- escape terminal mode
-vim.keymap.set("t", "<C-t>", "<C-\\><C-n>") 
+vim.keymap.set("t", "<C-t>", "<C-\\><C-n>")
 
 -- quickfix list remap
-vim.keymap.set("n", "<C-j>", "<cmd>cnext<cr>") 
-vim.keymap.set("n", "<C-k>", "<cmd>cprev<cr>") 
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<cr>")
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<cr>")
 
